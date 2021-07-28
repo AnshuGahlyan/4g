7d1a3b3caf9db93f37fbfcf9dab29a05c3c25bbb
AnshuGahlyan <71934013+AnshuGahlyan@users.noreply.github.com>
2021-07-28 21:02:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+
+firebase.auth().onAuthStateChanged(function(user) {
+
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+  //  location. replace("https://www.google.com");
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+  textSize(20);
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : ";
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+  location. replace("https://www.google.com");
+}
